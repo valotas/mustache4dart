895b929c0b79f5ffbbc2e1d95e3deece2b6bbdf5
Yoryos Valotasios <valotas@gloman.gr>
2012-12-02 01:26:49 +0100

    Implemeted a simple class that analyzes the given template in to tokens that we can afterwards use

--- src/Mustache.dart
+++ src/Mustache.dart
@@ -1,6 +1,27 @@
 library mustache4dart;
 
+import 'dart:core';
 
 class Mustache {
+ static String render(String template, context) {
+   _Template t = new _Template(template);
+   return null;
+ }
+}
+
+class _Template {
+  static final RegExp _EXP = new RegExp("(^\}\}).|\{\{([^\}]+)\}\}", multiLine: true);
+  List<String> _tokens = [];
   
+  _Template(String template) {
+    print(template);
+    num lastStart = 0;
+    _EXP.allMatches(template).forEach((m) {
+      _tokens.add(template.substring(lastStart, m.start));
+      _tokens.add(m[0]);
+      lastStart = m.end;
+      print("0: ${m[0]}, 1: ${m[1]}, 2: ${m[2]}, start: ${m.start}, end: ${m.end}");
+    });
+    print(_tokens);
+  }
 }
