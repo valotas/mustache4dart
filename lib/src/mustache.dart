5b7eb64d0862c9583f2e2910dd9b1cdc89a05ef1
Yoryos Valotasios <valotas@gloman.gr>
2012-12-23 16:19:39 +0100

    Instead of a forEach function call, we make us of a for-in construct to iterate the Tokens within a template

--- lib/src/mustache.dart
+++ lib/src/mustache.dart
@@ -5,9 +5,9 @@ class Mustache {
     _Template tmpl = new _Template(template);
     MustacheContext ctx = new MustacheContext(context);
     StringBuffer result = new StringBuffer();
-    tmpl.forEach((t) {
+    for (_Token t in tmpl) {
       result.add(t.apply(ctx));
-    });
+    }
     return result.toString();
   }
 }
