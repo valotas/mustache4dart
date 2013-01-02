6ce2160d7e2580a9c57fe1deaaf9cf6208638ac6
Yoryos Valotasios <valotas@gloman.gr>
2013-01-02 12:38:47 +0100

    Modifications to the Mustache in order to cache compiled templates

--- lib/src/mustache.dart
+++ lib/src/mustache.dart
@@ -1,8 +1,14 @@
 part of mustache4dart;
 
 class Mustache {
+  final Map<String, _Template> templates = {};
+  
   String render(String template, Object context) {
-    _Template tmpl = new _Template(template);
+    _Template tmpl = templates[template];
+    if (tmpl == null) {
+      tmpl = new _Template(template);
+      templates[template] = tmpl;
+    }
     MustacheContext ctx = new MustacheContext(context);
     StringBuffer result = new StringBuffer();
     for (_Token t in tmpl) {
