a7a5ff02028409595654226dd16e957250f24be7
Yoryos Valotasios <valotas@gloman.gr>
2013-01-04 11:55:30 +0100

    Added test for a section with a list

--- test/mustache_tests.dart
+++ test/mustache_tests.dart
@@ -14,4 +14,5 @@ void main() {
   test('No html escaped with & at the beginning of a key', () => expect(m.render('Not escaped: {{& html}}', {'html': '!@#\$%^&*()?<>'}), 'Not escaped: !@#\$%^&*()?<>'));
   test('Simple non existing section', () => expect(m.render('Shown. {{#nothin}}Never shown!{{/nothin}}', {'person': true}), 'Shown. '));  
   test('Simple existing section', () => expect(m.render('Shown: {{#person}}Yes, there is a person{{/person}}', {'person': true}), 'Shown: Yes, there is a person'));
+  test('Existing section with list', () => expect(m.render('Persons: {{#persons}}{{name}},{{/persons}}', {'persons': [{'name': 'name1'}, {'name': 'name2'}] }), 'Persons: name1,name2,'));
 }
