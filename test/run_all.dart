a9b20170eaacafe1e640d32231c2d4e73817fb1d
Yoryos Valotasios <valotas@gloman.gr>
2013-02-14 23:39:17 +0100

    Simplified the run_all.dart

--- test/run_all.dart
+++ test/run_all.dart
@@ -1,5 +1,3 @@
-library test.run_all;
-
 import 'package:unittest/compact_vm_config.dart';
 import 'package:unittest/unittest.dart';
 
@@ -7,16 +5,6 @@ import 'mustache_context_tests.dart' as mustache_context_tests;
 import 'mustache_tests.dart' as mustache_tests;
 
 main() {
-  var args = new Options().arguments;
-  var pattern = new RegExp(args.length > 0 ? args[0] : '.');
-  useCompactVMConfiguration();
-
-  void addGroup(testFile, testMain) {
-    if (pattern.hasMatch(testFile)) {
-      group(testFile.replaceAll('_test.dart', ':'), testMain);
-    }
-  }
-
-  addGroup('mustache_context_tests.dart', mustache_context_tests.main);
-  addGroup('mustache_tests.dart', mustache_tests.main);
+  mustache_context_tests.main();
+  mustache_tests.main();
 }
