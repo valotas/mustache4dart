5b81739d6986b52ba224a715fc89baef1d99fcbb
Yoryos Valotasios <valotas@gloman.gr>
2013-02-14 23:40:04 +0100

    Simplified the run.sh

--- test/run.sh
+++ test/run.sh
@@ -4,11 +4,11 @@
 set -e
 
 # TODO(sigmund): replace with a real test runner
-DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
+#DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
 
 # Note: dart_analyzer needs to be run from the root directory for proper path
 # canonicalization.
-pushd $DIR/..
+#pushd $DIR/..
 #echo Analyzing library for warnings or type errors
 #dart_analyzer --fatal-warnings --fatal-type-errors lib/*.dart \
 #  || echo -e "Ignoring analyzer errors (rtbug.com/8132r
