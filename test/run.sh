4233406882b77e9c4714a81c10207d93176bc916
Yoryos Valotasios <valotas@gloman.gr>
2013-02-14 23:28:17 +0100

    modified the run.sh as shown at the html5lib for dart

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# bail on error
 set -e
 
-dart --enable-type-checks --enable-asserts run_all.dart
+# TODO(sigmund): replace with a real test runner
+DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
+
+# Note: dart_analyzer needs to be run from the root directory for proper path
+# canonicalization.
+pushd $DIR/..
+echo Analyzing library for warnings or type errors
+dart_analyzer --fatal-warnings --fatal-type-errors lib/*.dart \
+  || echo -e "Ignoring analyzer errors (rtbug.com/8132r
+pd
+
+dart --enable-type-checks --enable-asserts test/run_all.dart $@
