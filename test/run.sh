6121ca0606e53fad7351c6a32249ec40393426be
Yoryos Valotasios <valotas@gloman.gr>
2013-02-14 23:31:23 +0100

    Deactivated the dart_analyzer for the moment

--- test/run.sh
+++ test/run.sh
@@ -9,9 +9,9 @@ DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
 # Note: dart_analyzer needs to be run from the root directory for proper path
 # canonicalization.
 pushd $DIR/..
-echo Analyzing library for warnings or type errors
-dart_analyzer --fatal-warnings --fatal-type-errors lib/*.dart \
-  || echo -e "Ignoring analyzer errors (rtbug.com/8132r
-pd
+#echo Analyzing library for warnings or type errors
+#dart_analyzer --fatal-warnings --fatal-type-errors lib/*.dart \
+#  || echo -e "Ignoring analyzer errors (rtbug.com/8132r
+#pd
 
 dart --enable-type-checks --enable-asserts test/run_all.dart $@
