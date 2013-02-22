e3b520f8f8e7eef51611c614ac586d7c86d52138
Yoryos Valotasios <valotas@gloman.gr>
2013-02-22 21:56:49 +0100

    Removed the Mustache class pushing the render function to a top level one

--- lib/src/mustache.dart
+++ lib/src/mustache.dart
@@ -1,17 +1,9 @@
 part of mustache4dart;
 
-class Mustache {
-  final Map<String, _Template> templates = {};
-  
-  String render(String template, Object context) {
-    _Template tmpl = templates[template];
-    if (tmpl == null) {
-      tmpl = new _Template(template);
-      templates[template] = tmpl;
-    }
-    MustacheContext ctx = new MustacheContext(context);
-    StringBuffer result = new StringBuffer();
-    tmpl.forEach((t) => result.add(t.apply(ctx)));
-    return result.toString();
-  }
+String render(String template, Object context) {
+  _Template tmpl = new _Template(template);
+  MustacheContext ctx = new MustacheContext(context);
+  StringBuffer result = new StringBuffer();
+  tmpl.forEach((t) => result.add(t.apply(ctx)));
+  return result.toString();
 }
--- test/mustache_tests.dart
+++ test/mustache_tests.dart
@@ -4,21 +4,20 @@ import 'package:/unittest/unittest.dart';
 import 'package:mustache4dart/mustache4dart.dart';
 
 void main() {
-  Mustache m = new Mustache();
-  test('Contextless template', () => expect(m.render('Ένα φανταστικό template', null), 'Ένα φανταστικό template'));
-  test('Simple template with map context', () => expect(m.render('Hello {{name}}!', {'name': 'George'}), 'Hello George!'));
-  test('Html escaped output', () => expect(m.render('Escaped: {{html}}', {'html': '!@#\$%^&*()?<>'}), 'Escaped: !@#\$%^&amp;*()?&lt;&gt;'));
-  test('No html escaped with tripple brackets', () => expect(m.render('Not escaped: {{{html}}}', {'html': '!@#\$%^&*()?<>'}), 'Not escaped: !@#\$%^&*()?<>'));
-  test('No html escaped with & at the beginning of a key', () => expect(m.render('Not escaped: {{& html}}', {'html': '!@#\$%^&*()?<>'}), 'Not escaped: !@#\$%^&*()?<>'));
-  test('Simple non existing section', () => expect(m.render('Shown. {{#nothin}}Never shown!{{/nothin}}', {'person': true}), 'Shown. '));
-  test('False value', () => expect(m.render('Shown. {{#show}}Never shown!{{/show}}', {'show': false}), 'Shown. '));
-  test('Empty list', () => expect(m.render('Shown. {{#list}}Never shown!{{/list}}', {'list': []}), 'Shown. '));
-  test('True value', () => expect(m.render('Shown? {{#show}}Yes it is shown!{{/show}}', {'show': true}), 'Shown? Yes it is shown!'));
-  test('Simple existing section', () => expect(m.render('Shown: {{#person}}Yes, there is a person{{/person}}', {'person': true}), 'Shown: Yes, there is a person'));
-  test('Existing section with list', () => expect(m.render('Persons: {{#persons}}{{name}},{{/persons}}', {'persons': [{'name': 'name1'}, {'name': 'name2'}] }), 'Persons: name1,name2,'));
-  test('Inverted section', () => expect(m.render('Persons: {{#persons}}{{name}},{{/persons}}{{^persons}}none!{{/persons}}', {}), 'Persons: none!'));
-  test('Simple lambda value', () => expect(m.render('{{#ff}}{{name}} is awesome{{/ff}}', {'name': 'George', 'ff': (t) => "$t!!!"}), '{{name}} is awesome!!!'));
-  test('Inverted section with lambda content', () => expect(m.render('Persons: {{#persons}}{{name}},{{/persons}}{{^persons}}{{#format}}none{{/format}}{{/persons}}', {'format': (t) => "$t!!!"}), 'Persons: none!!!'));
-  test('Simple comment', () => expect(m.render('{{! this is a comment}}Ένα φανταστικό template', null), 'Ένα φανταστικό template'));
-  test('Comment with in a lambda', () => expect(m.render('{{#format}}{{! ignore this}}none{{/format}}', {'format': (t) => "$t!!!"}), '{{! ignore this}}none!!!'));
+  test('Contextless template', () => expect(render('Ένα φανταστικό template', null), 'Ένα φανταστικό template'));
+  test('Simple template with map context', () => expect(render('Hello {{name}}!', {'name': 'George'}), 'Hello George!'));
+  test('Html escaped output', () => expect(render('Escaped: {{html}}', {'html': '!@#\$%^&*()?<>'}), 'Escaped: !@#\$%^&amp;*()?&lt;&gt;'));
+  test('No html escaped with tripple brackets', () => expect(render('Not escaped: {{{html}}}', {'html': '!@#\$%^&*()?<>'}), 'Not escaped: !@#\$%^&*()?<>'));
+  test('No html escaped with & at the beginning of a key', () => expect(render('Not escaped: {{& html}}', {'html': '!@#\$%^&*()?<>'}), 'Not escaped: !@#\$%^&*()?<>'));
+  test('Simple non existing section', () => expect(render('Shown. {{#nothin}}Never shown!{{/nothin}}', {'person': true}), 'Shown. '));
+  test('False value', () => expect(render('Shown. {{#show}}Never shown!{{/show}}', {'show': false}), 'Shown. '));
+  test('Empty list', () => expect(render('Shown. {{#list}}Never shown!{{/list}}', {'list': []}), 'Shown. '));
+  test('True value', () => expect(render('Shown? {{#show}}Yes it is shown!{{/show}}', {'show': true}), 'Shown? Yes it is shown!'));
+  test('Simple existing section', () => expect(render('Shown: {{#person}}Yes, there is a person{{/person}}', {'person': true}), 'Shown: Yes, there is a person'));
+  test('Existing section with list', () => expect(render('Persons: {{#persons}}{{name}},{{/persons}}', {'persons': [{'name': 'name1'}, {'name': 'name2'}] }), 'Persons: name1,name2,'));
+  test('Inverted section', () => expect(render('Persons: {{#persons}}{{name}},{{/persons}}{{^persons}}none!{{/persons}}', {}), 'Persons: none!'));
+  test('Simple lambda value', () => expect(render('{{#ff}}{{name}} is awesome{{/ff}}', {'name': 'George', 'ff': (t) => "$t!!!"}), '{{name}} is awesome!!!'));
+  test('Inverted section with lambda content', () => expect(render('Persons: {{#persons}}{{name}},{{/persons}}{{^persons}}{{#format}}none{{/format}}{{/persons}}', {'format': (t) => "$t!!!"}), 'Persons: none!!!'));
+  test('Simple comment', () => expect(render('{{! this is a comment}}Ένα φανταστικό template', null), 'Ένα φανταστικό template'));
+  test('Comment with in a lambda', () => expect(render('{{#format}}{{! ignore this}}none{{/format}}', {'format': (t) => "$t!!!"}), '{{! ignore this}}none!!!'));
 }
