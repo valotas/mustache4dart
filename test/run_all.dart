fea5e3df7a1a7e25f320750eda9d0ff8ad8f3677
Yoryos Valotasios <valotas@gloman.gr>
2013-02-23 16:44:36 +0100

    Added the specs to the group of the tests to run

--- test/run_all.dart
+++ test/run_all.dart
@@ -3,8 +3,10 @@ import 'package:unittest/unittest.dart';
 
 import 'mustache_context_tests.dart' as mustache_context_tests;
 import 'mustache_tests.dart' as mustache_tests;
+import 'mustache_specs.dart' as specs;
 
 main() {
   mustache_context_tests.main();
   mustache_tests.main();
+  specs.main();
 }
