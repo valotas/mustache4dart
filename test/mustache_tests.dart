610df1341570b6a5f9bd216f73a4f14c6b469fe7
Yoryos Valotasios <valotas@gmail.com>
2013-03-09 12:08:13 +0100

    Better performance tests

--- test/mustache_tests.dart
+++ test/mustache_tests.dart
@@ -22,12 +22,29 @@ void main() {
   });
   
   group('Performance tests', () {
-    var tmpl = '{{#a}}{{one}}{{#b}}-{{one}}{{two}}{{#c}}-{{one}}{{two}}{{three}}{{#d}}-{{one}}{{two}}{{three}}{{four}}{{/d}}{{/c}}{{/b}}{{/a}}';
-    var map = {'a': {'one': 1}, 'b': {'two': 2}, 'c': {'three': 3}, 'd': {'four': 4}};
-    var d = duration(100, () => render(tmpl, map));
+    var tmpl = '{{#a}}{{one}}{{#b}}-{{one}}{{two}}{{#c}}-{{one}}{{two}}{{three}}{{#d}}-{{one}}{{two}}{{three}}{{four}}{{#e}}{{one}}{{two}}{{three}}{{four}}{{/e}}{{/d}}{{/c}}{{/b}}{{/a}}';
+    StringBuffer buf = new StringBuffer(tmpl);
+    for (int i = 0; i < 10; i++) {
+      buf.write('dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd');
+      buf.write(tmpl);
+    }
+    tmpl = buf.toString();
     
+    var map = {'a': {'one': 1}, 
+               'b': {'two': 2}, 
+               'c': {'three': 3}, 
+               'd': {'four': 4}, 
+               'e': false};
     var ctmpl = compile(tmpl);
+    
+    var warmup = duration(100, () => "${ctmpl(map)}--${render(tmpl, map)}");
+    print("Warmup rendering of template with length ${tmpl.length} took ${warmup}millis");
+    
+    var d = duration(100, () => render(tmpl, map));
+    print("100 iterations of uncompiled rendering took ${d}millis");
+    
     var d2 = duration(100, () => ctmpl(map));
+    print("100 iterations of compiled rendering tool ${d2}millis");
     
     test('Compiled templates should be at least 2 times faster', () => expect(d2 < (d/2), isTrue));
   });
@@ -39,5 +56,5 @@ num duration(int reps, f()) {
     f();
   }
   var end = new DateTime.now();
-  return end.millisecond - start.millisecond;
+  return end.millisecondsSinceEpoch - start.millisecondsSinceEpoch;
 }
