c072646c75ec776b55fa625f1c9fa69bf4a0b034
Yoryos Valotasios <valotas@gmail.com>
2013-05-01 23:05:19 +0200

    Added failing test for #11

--- test/mustache_issues.dart
+++ test/mustache_issues.dart
@@ -6,5 +6,6 @@ import 'package:mustache4dart/mustache4dart.dart';
 void main() {
   group('mustache4dart issues', () {    
     test('#9', () => expect(render("{{#sec}}[{{var}}]{{/sec}}", {'sec': 42}), '[]'));
+    test('#11', () => expect(render("{{#sec}}[{{var}}]{{/somethingelse}}", {'sec': 42}), throwsException));
   });
 }
