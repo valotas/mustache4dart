65a7660b9a112bc67cbe6028c593a8c450e6c7dc
Yoryos Valotasios <valotas@gmail.com>
2013-05-01 11:00:20 +0200

    Add all the issue tests as part of the test suite

--- test/run_all.dart
+++ test/run_all.dart
@@ -4,9 +4,11 @@ import 'package:unittest/unittest.dart';
 import 'mustache_context_tests.dart' as mustache_context_tests;
 import 'mustache_tests.dart' as mustache_tests;
 import 'mustache_specs.dart' as specs;
+import 'mustache_issues.dart' as issues;
 
 main() {
   mustache_context_tests.main();
   mustache_tests.main();
   specs.main();
+  issues.main();
 }
