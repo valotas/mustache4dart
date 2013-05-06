dc20bf6e582cea19777b3393162fb419a0faf51a
Yoryos Valotasios <valotas@gmail.com>
2013-05-07 00:08:33 +0200

    Removed the stupid _computedNext

--- lib/src/tokens.dart
+++ lib/src/tokens.dart
@@ -315,33 +315,28 @@ class _EscapeHtmlToken extends _ExpressionToken {
 class _StartSectionToken extends _ExpressionToken implements _StandAloneLineCapable {
   final Delimiter delimiter;
   _EndSectionToken endSection;
-  _Token _computedNext;
   
   _StartSectionToken(String val, this.delimiter) : super.withSource(val, null);
 
   //Override the next getter
-  _Token get next => _computedNext != null ? _computedNext.next : super.next;
+  _Token get next =>  endSection.next;
 
   apply(MustacheContext ctx) {
     var val = ctx[name];
     if (val == null) {
-      _computedNext = endSection;
       return EMPTY_STRING;
     }
     StringBuffer str = new StringBuffer();
     if (val is Function) { //apply the source to the given function
-      _computedNext = endSection;
       forEachUntilEndSection((_Token t) => str.write(t._source));
       //A lambda's return value should be parsed
       return render(val(str.toString()), ctx, delimiter: delimiter);
     }
     if (val is MustacheContext) { //apply the new context to each of the tokens until the end
-      _computedNext = endSection;
       forEachUntilEndSection((_Token t) => str.write(t.apply(val)));
       return str;
     }
     if (val is Iterable) {
-      _computedNext = endSection;
       val.forEach((v) {
         forEachUntilEndSection((_Token t) => str.write(t.apply(v)));
       });
@@ -380,7 +375,6 @@ class _InvertedSectionToken extends _StartSectionToken {
   _InvertedSectionToken(String val, Delimiter del) : super(val, del);
   
   apply(MustacheContext ctx) {
-    _computedNext = endSection;
     if (ctx[name] == null) {
       StringBuffer buf = new StringBuffer();
       forEachUntilEndSection((_Token t) {
