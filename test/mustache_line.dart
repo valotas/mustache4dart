08117850acc78c247a2887912a5a0c21521c0712
Yoryos Valotasios <valotas@gmail.com>
2013-05-18 22:06:29 +0200

    Simplified line tests by providing a factory method

--- test/mustache_line.dart
+++ test/mustache_line.dart
@@ -6,30 +6,32 @@ void main() {
   group('mustache4dart Line', () {
     var del = new Delimiter('{{', '}}');
 
+    newToken (String s) => new Token(s, null, del, null);
+
     test('Should not accept more tokens when it is full', () {
-      var l = new Line(new Token('Random text', null, del, null));
-      l.add(new Token('Random text2', null, del, null));
-      l.add(new Token(NL, null, del, null));
-      expect(() => l.add(new Token('Some more random text', null, del, null)), throwsStateError);
+      var l = new Line(newToken('Random text'));
+      l.add(newToken('Random text2'));
+      l.add(newToken(NL));
+      expect(() => l.add(newToken('Some more random text')), throwsStateError);
     });
 
     test('add method should return the right line to add more stuff', () {
-      var l = new Line(new Token('some text', null, del, null));
-      var l2 = l.add(new Token('some more text', null, del, null));
+      var l = new Line(newToken('some text'));
+      var l2 = l.add(newToken('some more text'));
       expect(l2, same(l));
-      var nl = l.add(new Token(NL, null, del, null));
+      var nl = l.add(newToken(NL));
       expect(nl, isNot(same(l)));
     });
 
     test('Should not be standalone if it contains a string token', () {
-      var l = new Line(new Token('Some text!', null, del, null));
+      var l = new Line(newToken('Some text!'));
       expect(l.standAlone, isFalse);
     });
 
     test("Expression tokens should be considered stand alone capable", () {
-      var l = new Line(new Token(' ', null, del, null));
-      l.add(new Token(' ', null, del, null));
-      l.add(new Token('{{/xxx}}', null, del, null));
+      var l = new Line(newToken(' '));
+      l.add(newToken(' '));
+      l.add(newToken('{{/xxx}}'));
       expect(l.standAlone, isTrue);
     });
   });
