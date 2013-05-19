c6fc4194259b738e171d61df0e6dc8179b760fbf
Yoryos Valotasios <valotas@gmail.com>
2013-05-19 13:14:05 +0200

    Added test for #10

--- test/mustache_issues.dart
+++ test/mustache_issues.dart
@@ -6,6 +6,7 @@ import 'package:mustache4dart/mustache4dart.dart';
 void main() {
   group('mustache4dart issues', () {    
     test('#9', () => expect(render("{{#sec}}[{{var}}]{{/sec}}", {'sec': 42}), '[]'));
+    test('#10', () => expect(render('|\n{{#bob}}\n{{/bob}}\n|', {'bob': []}), '|\n|'));
     test('#11', () => expect(() => render("{{#sec}}[{{var}}]{{/somethingelse}}", {'sec': 42}), throwsFormatException));
   });
 }
