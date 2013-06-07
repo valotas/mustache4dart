ce0bc54960f5c54b734ac5b4731837c550f69ac6
Yoryos Valotasios <valotas@gmail.com>
2013-06-07 21:54:39 +0200

    run.sh improvements

--- test/run.sh
+++ test/run.sh
@@ -4,13 +4,13 @@
 set -e
 
 # TODO(sigmund): replace with a real test runner
-#DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
+DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
 
 # Note: dart_analyzer needs to be run from the root directory for proper path
 # canonicalization.
-#pushd $DIR/..
+pushd $DIR/..
 #echo Analyzing library for warnings or type errors
-dartanalyzer --fatal-warnings --fatal-type-errors lib/*.dart
-#  || echo -e "Ignoring analyzer errors (rtbug.com/8132r
+dartanalyzer --fatal-warnings --fatal-type-errors lib/*.dart \
+  || echo -e "Ignoring analyzer errors"
 #pd
 dart --enable-type-checks --enable-asserts test/run_all.dart $@
