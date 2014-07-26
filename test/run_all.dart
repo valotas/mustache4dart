02018bd67f420855978fdf6458b8e63ac5cf6c72
Yoryos Valotasios <valotas@gmail.com>
2014-07-26 18:20:28 +0200

    Removed unused imports

--- test/run_all.dart
+++ test/run_all.dart
@@ -1,6 +1,3 @@
-import 'package:unittest/compact_vm_config.dart';
-import 'package:unittest/unittest.dart';
-
 import 'mustache_context_tests.dart' as mustache_context_tests;
 import 'mustache_tests.dart' as mustache_tests;
 import 'mustache_specs.dart' as specs;
