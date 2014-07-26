2c2ee2d9f29c02c882b259457f2c4304ab3e15cd
Yoryos Valotasios <valotas@gmail.com>
2014-07-26 19:04:29 +0200

    Make sure that tests are running again

--- test/mustache_specs.dart
+++ test/mustache_specs.dart
@@ -6,55 +6,61 @@ import 'package:unittest/unittest.dart';
 import 'package:mustache4dart/mustache4dart.dart';
 
 main() {
+  defineTests();
+}
+
+defineTests () {
   var specs_dir = new Directory('spec/specs');
   specs_dir
     .listSync()
     .forEach((File f) {
       var filename = f.path;
       if (shouldRun(filename)) {
-        f.readAsString(encoding: UTF8)
-        .then((text) {
-          var json = JSON.decode(text);
-          var tests = json['tests'];
-          filename = filename.substring(filename.lastIndexOf('/') + 1);
-          group("Specs of $filename", () {
-            
-            //Make sure that we reset the state of the Interpolation - Multiple Calls test
-            //as for some reason dart can run the group more than once causing the test
-            //to fail the second time it runs
-            tearDown (() =>lambdas['Interpolation - Multiple Calls'].reset());
-            
-            tests.forEach( (t) {
-              var testDescription = new StringBuffer(t['name']);
-              testDescription.write(': ');
-              testDescription.write(t['desc']);
-              var template = t['template'];
-              var data = t['data'];
-              var templateOneline = template.replaceAll('\n', '\\n').replaceAll('\r', '\\r');
-              var reason = new StringBuffer("Could not render right '''$templateOneline'''");
-              var expected = t['expected'];
-              var partials = t['partials'];
-              var partial = (String name) {
-                if (partials == null) {
-                  return null;
-                }
-                return partials[name];
-              };
-              
-              //swap the data.lambda with a dart real function
-              if (data['lambda'] != null) {
-                data['lambda'] = lambdas[t['name']];
-              }
-              reason.write(" with '$data'");
-              if (partials != null) {
-                reason.write(" and partial: $partials");
-              }
-              test(testDescription.toString(), () => expect(render(template, data, partial: partial), expected, reason: reason.toString())); 
-            });            
-          });
-        });
+        var text = f.readAsStringSync(encoding: UTF8);
+        _defineGroupFromFile(filename, text);
       }
     });
+}
+
+_defineGroupFromFile(filename, text) {
+  var json = JSON.decode(text);
+  var tests = json['tests'];
+  filename = filename.substring(filename.lastIndexOf('/') + 1);
+  group("Specs of $filename", () {
+    
+    //Make sure that we reset the state of the Interpolation - Multiple Calls test
+    //as for some reason dart can run the group more than once causing the test
+    //to fail the second time it runs
+    tearDown (() =>lambdas['Interpolation - Multiple Calls'].reset());
+    
+    tests.forEach( (t) {
+      var testDescription = new StringBuffer(t['name']);
+      testDescription.write(': ');
+      testDescription.write(t['desc']);
+      var template = t['template'];
+      var data = t['data'];
+      var templateOneline = template.replaceAll('\n', '\\n').replaceAll('\r', '\\r');
+      var reason = new StringBuffer("Could not render right '''$templateOneline'''");
+      var expected = t['expected'];
+      var partials = t['partials'];
+      var partial = (String name) {
+        if (partials == null) {
+          return null;
+        }
+        return partials[name];
+      };
+      
+      //swap the data.lambda with a dart real function
+      if (data['lambda'] != null) {
+        data['lambda'] = lambdas[t['name']];
+      }
+      reason.write(" with '$data'");
+      if (partials != null) {
+        reason.write(" and partial: $partials");
+      }
+      test(testDescription.toString(), () => expect(render(template, data, partial: partial), expected, reason: reason.toString())); 
+    });            
+  });
 }
 
 bool shouldRun(String filename) {
