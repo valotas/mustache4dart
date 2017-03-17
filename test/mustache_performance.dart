8ae8a2a6436594b5a8369fa2ef52484914e9ed33
Georgios Valotasios <valotas@gmail.com>
2017-03-17 19:33:05 +0100

    Minor improvements
    - make use of final instead of var
    - decreased iterations
    - formatted the template

--- test/mustache_performance.dart
+++ test/mustache_performance.dart
@@ -1,48 +1,51 @@
 import 'package:mustache4dart/mustache4dart.dart';
 
-const ITERATIONS = 10000;
+const ITERATIONS = 1000;
 
 main() {
-  var tmpl = createTemplate();
+  final tmpl = createTemplate();
 
-  var map = {
+  final map = {
     'a': {'one': 1},
     'b': {'two': 2},
     'c': {'three': 3},
     'd': {'four': 4},
     'e': false
   };
-  var ctmpl = compile(tmpl);
+  final ctmpl = compile(tmpl);
 
-  var warmup = duration(() => "${ctmpl(map)}--${render(tmpl, map)}");
+  final warmup = duration(() => "${ctmpl(map)}--${render(tmpl, map)}");
   print(
       "Warmup rendering of template with length ${tmpl.length} took ${warmup}millis");
 
-  var d = duration(() => render(tmpl, map));
+  final d = duration(() => render(tmpl, map));
   print("Uncompiled rendering took ${d}millis");
 
-  var d2 = duration(() => ctmpl(map));
+  final d2 = duration(() => ctmpl(map));
   print("Compiled rendering took ${d2}millis");
 
   print("Score relation: ${d2/d}");
 }
 
 createTemplate() {
-  var tmpl =
-      '{{#a}}{{one}}{{#b}}-{{one}}{{two}}{{#c}}-{{one}}{{two}}{{three}}{{#d}}-{{one}}{{two}}{{three}}{{four}}{{#e}}{{one}}{{two}}{{three}}{{four}}{{/e}}{{/d}}{{/c}}{{/b}}{{/a}}';
-  StringBuffer buf = new StringBuffer(tmpl);
+  final tmpl =
+      '''{{#a}}{{one}}{{#b}}-{{one}}{{two}}{{#c}}-{{one}}{{two}}{{three}}
+        {{#d}}-{{one}}{{two}}{{three}}{{four}}{{#e}}{{one}}{{two}}{{three}}
+        {{four}}{{/e}}{{/d}}{{/c}}{{/b}}{{/a}}''';
+  final buf = new StringBuffer(tmpl);
   for (int i = 0; i < 10; i++) {
     buf.write('dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd');
     buf.write(tmpl);
   }
   return buf.toString();
+
 }
 
 num duration(f()) {
-  var start = new DateTime.now();
+  final start = new DateTime.now();
   for (int i = 0; i < ITERATIONS; i++) {
     f();
   }
-  var end = new DateTime.now();
+  final end = new DateTime.now();
   return end.millisecondsSinceEpoch - start.millisecondsSinceEpoch;
 }
