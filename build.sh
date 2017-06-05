e438bca3f81b591c2025c6f09a39d4a6bf436e4c
Georgios Valotasios <valotas@gmail.com>
2017-06-05 22:11:25 +0200

    Adapt build script

--- build.sh
+++ build.sh
@@ -29,4 +29,6 @@ if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
     test/mustache_all.dart
 fi
 
-pub run test test/mustache_context_test.dart -p chrome
+which google-chrome
+which firefox
+pub run test test/mustache_context_test.dart -p chrome,firefox
