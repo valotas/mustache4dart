b9bb1a7d529a1b36fb65cb4ec49051c3dfa01c3a
Georgios Valotasios <valotas@gmail.com>
2017-08-21 16:50:13 +0200

    introduced a proper mustache tokenizer

--- lib/src/parser.dart
+++ lib/src/parser.dart
@@ -1,13 +1,101 @@
+import "dart:async";
+
+final int _LF = "\n".runes.first;
+final int _CR = "\r".runes.first;
+final int _OPEN = "{".runes.first;
+final int _CLOSE = "}".runes.first;
+
+Stream<Token> tokenize(String input) {
+  return _tokenize(new Stream.fromIterable([input]));
+}
+
+Stream<Token> _tokenize(Stream<String> chunks) {
+  return chunks
+      .expand((chunk) => chunk.runes)
+      .transform(new _TokenizeTransformer());
+}
+
 enum TokenType {
-  Line
+  Literal,
+  NewLine,
+  Open,
+  Close
 }
 
 class Token {
   final TokenType type;
+  final List<int> codeUnits;
+  final String value;
   final int col;
   final int line;
+
+  Token(this.type, this.codeUnits, {this.col, line}) :
+        this.value = new String.fromCharCodes(codeUnits),
+        this.line = type == TokenType.NewLine ? line - 1 : line;
 }
 
-List<Token> parse(String tmpl) {
-  return [];
+class _TokenizerSink extends EventSink<int> {
+  final EventSink<Token> _out;
+
+  TokenType _tokenType = null;
+  List<int> _buffer = [];
+  int _currColumn = 1;
+  int _currLine = 1;
+  int _lastFlushColumn = 1;
+
+  _TokenizerSink(this._out);
+
+  void add(int unit) {
+    if (unit == _LF || unit == _CR) {
+      _addNewLine(unit);
+    } else if (unit == _OPEN) {
+      _addToken(TokenType.Open);
+    } else if (unit == _CLOSE) {
+      _addToken(TokenType.Close);
+    } else {
+      _addToken(TokenType.Literal);
+    }
+    _currColumn++;
+    _buffer.add(unit);
+  }
+
+  void _addNewLine(unit) {
+    if (unit == _LF) {
+      _flush();
+      _currLine++;
+    }
+    _currColumn = 0;
+    _tokenType = TokenType.NewLine;
+  }
+
+  void _addToken(TokenType type) {
+    if (_tokenType != type) {
+      _flush();
+    }
+    _tokenType = type;
+  }
+
+  void _flush() {
+    if (_tokenType == null) {
+      return;
+    }
+    _out.add(new Token(_tokenType, _buffer, col: _lastFlushColumn, line: _currLine));
+    _lastFlushColumn = _currColumn;
+    _buffer = [];
+  }
+
+  void addError(e, [st]) {
+    _out.addError(e, st);
+  }
+
+  void close() {
+    _flush();
+    _out.close();
+  }
+}
+
+class _TokenizeTransformer implements StreamTransformer<int, Token> {
+  Stream<Token> bind(Stream<int> input) =>
+      new Stream.eventTransformed(input,
+              (EventSink sink) => new _TokenizerSink(sink));
 }
--- test/mustache_parser_test.dart
+++ test/mustache_parser_test.dart
@@ -2,11 +2,83 @@ import 'package:test/test.dart';
 import 'package:mustache4dart/src/parser.dart';
 
 void main() {
-  group('simple templates', () {
-    test('returns lines', () {
-      final tokens = parse("one!");
-      expect(tokens.length, greaterThan(0));
-      expect(tokens[0].type, TokenType.Line);
+  group('tokenize', () {
+    test('return literals', () async {
+      final tokens = await tokenize("one!").toList();
+      expect(tokens.length, 1);
+      expect(tokens[0].type, TokenType.Literal);
+      expect(tokens[0].value, "one!");
+    });
+
+    test('return newLines', () async {
+      final tokens = await tokenize("one\n").toList();
+      expect(tokens.length, 2);
+      expect(tokens[1].type, TokenType.NewLine);
+    });
+
+    test('return \n\r as one newLine token', () async {
+      final tokens = await tokenize("one\n\rtrow").toList();
+      expect(tokens.length, 3);
+      expect(tokens[1].type, TokenType.NewLine);
+    });
+
+    test('returns opening mustaches', () async {
+      final tokens = await tokenize("one {{m}}").toList();
+      expect(tokens.length, 4);
+      expect(tokens[1].type, TokenType.Open);
+    });
+
+    test('returns closing mustaches', () async {
+      final tokens = await tokenize("one {{m}}").toList();
+      expect(tokens.length, 4);
+      expect(tokens[3].type, TokenType.Close);
+    });
+
+    test('return the right line of the token', () async {
+      final tokens = await tokenize("one\ntwo\n\rthree").toList();
+      expect(tokens.length, 5);
+      expect(tokens[0].line, 1);
+      expect(tokens[1].line, 1);
+      expect(tokens[2].line, 2);
+      expect(tokens[3].line, 2);
+      expect(tokens[4].line, 3);
+    });
+
+    test('handles more than one line feed in a row', () async {
+      final tokens = await tokenize("one\n\n\r\nthree").toList();
+      expect(tokens.length, 5);
+      expect(tokens[0].line, 1);
+      expect(tokens[1].line, 1);
+      expect(tokens[2].line, 2);
+      expect(tokens[3].line, 3);
+      expect(tokens[4].line, 4);
+    });
+
+    test('return the column of the token', () async {
+      final tokens = await tokenize("one\ntwo\n\rthree").toList();
+      expect(tokens.length, 5);
+      expect(tokens[0].col, 1);
+      expect(tokens[1].col, 4);
+      expect(tokens[2].col, 1);
+      expect(tokens[3].col, 4);
+      expect(tokens[4].col, 1);
+    });
+
+    test('return a literal foreach line', () async {
+      final tokens = await tokenize("one\ntwo").toList();
+      expect(tokens.length, 3);
+      expect(tokens[0].type, TokenType.Literal);
+      expect(tokens[0].value, "one");
+      expect(tokens[0].col, 1);
+      expect(tokens[0].line, 1);
+      expect(tokens[1].type, TokenType.NewLine);
+      expect(tokens[1].value, "\n");
+      expect(tokens[1].col, 4);
+      expect(tokens[1].line, 1);
+      expect(tokens[2].type, TokenType.Literal);
+      expect(tokens[2].value, "two");
+      expect(tokens[2].col, 1);
+      expect(tokens[2].line, 2);
     });
   });
 }
