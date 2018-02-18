1ea553a18e7dcd7095cd0f935747c008bea4627e
Georgios Valotasios <valotas@gmail.com>
2018-02-18 18:48:50 +0100

    Reformat

--- test/mustache_all.dart
+++ test/mustache_all.dart
@@ -3,7 +3,8 @@ import "mustache_issues_test.dart" as issues_test;
 import "mustache_line_test.dart" as line_test;
 import "mustache_specs_test.dart" as specs_test;
 import "mustache_test.dart" as general_test;
-import "mustache_context_reflect_with_mirros_test.dart" as reflect_with_mirrors_test;
+import "mustache_context_reflect_with_mirros_test.dart"
+    as reflect_with_mirrors_test;
 
 void main() {
   context_test.main();
