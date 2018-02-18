1a5d84310074de78a2a7b97d45582c42693a2f92
Georgios Valotasios <valotas@gmail.com>
2018-02-18 12:11:27 +0100

    rename reflect tests to reflect_with_mirrors

--- test/mustache_all.dart
+++ test/mustache_all.dart
@@ -3,7 +3,7 @@ import "mustache_issues_test.dart" as issues_test;
 import "mustache_line_test.dart" as line_test;
 import "mustache_specs_test.dart" as specs_test;
 import "mustache_test.dart" as general_test;
-import "mustache_context_reflect_test.dart" as reflect_test;
+import "mustache_context_reflect_with_mirros_test.dart" as reflect_with_mirrors_test;
 
 void main() {
   context_test.main();
@@ -11,5 +11,5 @@ void main() {
   line_test.main();
   specs_test.main();
   general_test.main();
-  reflect_test.main();
+  reflect_with_mirrors_test.main();
 }
